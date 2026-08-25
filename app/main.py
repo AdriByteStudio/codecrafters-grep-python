6d1f1fd13acc8f04def28d945c63298dcabeac54
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-26 00:50:48 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -23,9 +23,14 @@ def match_at(input_line, pattern, pos):
             ii += 1
         elif pattern[pi] == '[':
             end = pattern.index(']', pi + 1)
-            chars = pattern[pi + 1:end]
-            if ii >= len(input_line) or input_line[ii] not in chars:
-                return False
+            if pattern[pi + 1] == '^':
+                chars = pattern[pi + 2:end]
+                if ii >= len(input_line) or input_line[ii] in chars:
+                    return False
+            else:
+                chars = pattern[pi + 1:end]
+                if ii >= len(input_line) or input_line[ii] not in chars:
+                    return False
             pi = end + 1
             ii += 1
         else:
